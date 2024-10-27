a90f76e904624babf6b631364cc499c764f944dc
Shoyas <md.nasiruddin4067@gmail.com>
2024-10-27 11:24:19 +0600

    ADD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome to Next.js with TypeScript!</h1>
+      <h1>This Next JS Project is using to implement Adsterra Ads Testing</h1>
+      <p>Because I've made a gaming website using Next JS, That website is ready to use Adsterra Ads, But I've not implement Adsterra with Next JS App before.</p>
     </div>
   );
 }
