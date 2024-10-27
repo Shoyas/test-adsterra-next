4633c5db1655cdd816da988cb51306c3e9575496
Shoyas <md.nasiruddin4067@gmail.com>
2024-10-27 11:16:27 +0600

    first commit

---
+++ src/app/shoyas/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShoyasPage = () => {
+    return (
+        <div>
+            <h1>This is shoyas page</h1>
+        </div>
+    );
+};
+
+export default ShoyasPage;
