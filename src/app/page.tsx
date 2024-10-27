8a5efd522aca1a7299ffca78739d59f4313320f2
Shoyas <md.nasiruddin4067@gmail.com>
2024-10-27 12:09:45 +0600

    [ADD] adterra add 468*60 test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Banner468_60 from "@/ui/adsterra/banner468_60";
 
 
 
@@ -6,6 +7,9 @@ export default function HomePage() {
     <div>
       <h1>This Next JS Project is using to implement Adsterra Ads Testing</h1>
       <p>Because I&apos;ve made a gaming website using Next JS, That website is ready to use Adsterra Ads, But I&apos;ve not implement Adsterra with Next JS App before.</p>
+      <div>
+        <Banner468_60 />
+      </div>
     </div>
   );
 }
