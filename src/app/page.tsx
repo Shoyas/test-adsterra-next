4b0397e6901aa56b5ab36dd3a848650be0938028
Shoyas <md.nasiruddin4067@gmail.com>
2024-10-27 18:05:24 +0600

    [DONE] add second ads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import Banner300_250 from "@/ui/adsterra/banner300_250";
 import Banner468_60 from "@/ui/adsterra/banner468_60";
 
 
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>This Next JS Project is using to implement Adsterra Ads Testing</h1>
-      <p>Because I&apos;ve made a gaming website using Next JS, That website is ready to use Adsterra Ads, But I&apos;ve not implement Adsterra with Next JS App before.</p>
+    <div className="grid grid-cols-4 gap-4">
+      <div>
+        <Banner300_250/>
+      </div>
+      <div>
+        <h1>This Next JS Project is using to implement Adsterra Ads Testing</h1>
+        <p>Because I&apos;ve made a gaming website using Next JS, That website is ready to use Adsterra Ads, But I&apos;ve not implement Adsterra with Next JS App before.</p>
+      </div>
       <div>
         <Banner468_60 />
       </div>
---
+++ src/ui/adsterra/banner300_250.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import { useEffect, useRef } from 'react';
+
+interface IAtOptions {
+    key: string;
+    format: string;
+    height: number | string;
+    width: number | string;
+    params: any;
+}
+
+const Banner300_250 = () => {
+    const banner = useRef<HTMLDivElement | null>(null);
+
+    const atOptions: IAtOptions = {
+		'key' : '863ed434b359def8498b93bd1e214e18',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+
+    useEffect(() => {
+        if (banner.current && !banner.current.firstChild) {
+            const conf = document.createElement('script');
+            const script = document.createElement('script');
+
+            conf.type = 'text/javascript';
+            conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
+
+            script.type = 'text/javascript';
+            script.src = `//www.highperformanceformat.com/${atOptions.key}/invoke.js`;
+            //!www.highperformanceformat.com/386795cf83ba9d1b8f890cf1de085519/invoke.js
+
+            banner.current.appendChild(conf);
+            banner.current.appendChild(script);
+
+            // Cleanup function to remove scripts when component unmounts
+            return () => {
+                if (banner.current) {
+                    banner.current.innerHTML = '';
+                }
+            };
+        }
+    }, [atOptions]);
+
+    return (
+        <div
+            className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center"
+            ref={banner}
+        ></div>
+    );
+};
+
+export default Banner300_250;
